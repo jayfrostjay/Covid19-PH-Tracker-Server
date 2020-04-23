4af6d2bfb8a1eeb403c93ba7e066b36f1c6d2a5d
Jayson Garcia <jayfrostgarcia@gmail.com>
2020-04-23 21:26:55 +0800

    Update added handlebars for http web access

--- app.js
+++ app.js
@@ -2,16 +2,10 @@ const path = require('path')
 require('dotenv').config({ path: path.join(__dirname, '.env') })
 
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const express = require('express')
-// const mongoose = require('mongoose')
 const logger = require('morgan')
 const nocache = require('nocache')
-const session = require('express-session')
-// const MongoStore = require('connect-mongo')(session)
-
-// require('./configs/database')
 
 const app_name = require('./package.json').name
 const debug = require('debug')(
@@ -19,54 +13,24 @@ const debug = require('debug')(
 )
 
 const app = express()
+const exphbs = require('express-handlebars')
 
 app.use(nocache())
-
-// Set "Access-Control-Allow-Origin" header
-// app.use(
-//   cors({
-//     origin: (origin, cb) => {
-//       cb(null, origin && origin.startsWith('http://localhost:'))
-//     },
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-//   })
-// )
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 
-// Set the public folder to "~/client/build/"
-// Example: http://localhost:5000/favicon.ico => Display "~/client/build/favicon.ico"
-app.use(express.static(path.join(__dirname, '../client/build')))
+// handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
-// Enable authentication using session + passport
-// app.use(
-//   session({
-//     secret: process.env.SESSION_SECRET || 'irongenerator',
-//     resave: true,
-//     saveUninitialized: true,
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//   })
-// )
-// require('./passport')(app)
-
-app.use('/api', require('./routes/index'))
-// app.use('/api', require('./routes/auth'))
 app.use('/api/covid', require('./routes/api/covid'))
 
-// For any routes that starts with "/api", catch 404 and forward to error handler
-app.use('/api/*', (req, res, next) => {
-  let err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-// For any other routes, redirect to the index.html file of React
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'))
-})
+// Homepage 
+app.get('/', (req, res) => res.render('index'))
+// Default 
+app.get('*', (req, res) => res.render('notfound'))
 
 // Error handler
 app.use((err, req, res, next) => {
